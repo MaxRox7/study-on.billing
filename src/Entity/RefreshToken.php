8f7d0652734bde4304f7c786ab5b776f77d448b1
Maxim <maxrox1904@gmail.com>
2025-06-20 01:44:40 +0300

    work 9

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken as BaseRefreshToken;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: 'Gesdinet\\JWTRefreshTokenBundle\\Entity\\RefreshTokenRepository')]
 #[ORM\Table(name: 'refresh_tokens')]
 class RefreshToken extends BaseRefreshToken
 {
